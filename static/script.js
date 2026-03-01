6624db929fae8bf9f2ccddd27ae57ec9a0a7f803
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-03-01 11:53:51 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,11 @@
 
+async function updateJackpotDisplay() {
+    const response = await fetch('/api/jackpot');
+    const result = await response.json();
+    if (result.status === 'ok') {
+        document.getElementById('jackpot-amount').innerText = `🎰 Jackpot: ${result.data.amount}`;
+    }
+}
+
+// Запускай обновление каждые 10 секунд
+setInterval(updateJackpotDisplay, 10000);
