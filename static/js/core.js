3d64c3df084a5bcaa7dcaa7a67f9c90cf271739c
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-03-08 17:42:36 +0300

    Delete static/js directory

--- static/js/core.js
+++
@@ -1,8 +0,0 @@
-// Клиентский скрипт - выполняется только в браузере!
-const tg = window.Telegram.WebApp;
-tg.expand();
-
-const socket = io(); // Подключение к нашему Node.js серверу
-
-// Логика инициализации...
-console.log("Neural Pulse Core Initialized");
