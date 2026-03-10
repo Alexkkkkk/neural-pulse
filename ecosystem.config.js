5b1a36cc2766520f814f37f629f044d3dbb78526
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-03-10 19:21:57 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,16 +5,11 @@ module.exports = {
     instances: 1,
     exec_mode: "fork",
     autorestart: true,
-    watch: false,
-    max_memory_restart: '400M',
     wait_ready: true,
-    listen_timeout: 5000,
+    listen_timeout: 8000,
     env: {
       NODE_ENV: "production",
       PORT: 3000
-    },
-    log_date_format: "YYYY-MM-DD HH:mm:ss",
-    error_file: "logs/err.log",
-    out_file: "logs/out.log"
+    }
   }]
 }
