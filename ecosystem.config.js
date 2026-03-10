998ac8a603cf2e1f063cef34739c1a75b0dc328b
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-03-10 19:56:32 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,16 @@
 module.exports = {
   apps: [{
-    name: "np-core",
+    name: "neural-pulse",
     script: "http-wrapper.js",
-    instances: 1,
-    exec_mode: "fork",
     autorestart: true,
     wait_ready: true,
-    listen_timeout: 10000,
     max_memory_restart: '400M',
     env: {
       NODE_ENV: "production",
       PORT: 3000
     },
-    log_date_format: "YYYY-MM-DD HH:mm:ss",
     error_file: "logs/err.log",
-    out_file: "logs/out.log"
+    out_file: "logs/out.log",
+    log_date_format: "YYYY-MM-DD HH:mm:ss"
   }]
 }
