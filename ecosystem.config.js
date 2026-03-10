1bc6e930a2715dbc669b745bea47b7aa43e4d6a1
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-03-10 21:18:24 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,1 @@
-module.exports = {
-  apps: [{
-    name: "pulse-bot-main",
-    script: "server.js",
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '450M',
-    env: {
-      NODE_ENV: "production",
-      PORT: 3000
-    }
-  }]
-}
+
