8338ef4666dab9e71c595875acf0984bc7c5f244
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-03-10 21:18:46 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,10 @@
 
+module.exports = {
+  apps: [{
+    name: "neural-pulse",
+    script: "server.js",
+    autorestart: true,
+    max_memory_restart: '450M',
+    env: { NODE_ENV: "production", PORT: 3000 }
+  }]
+}
