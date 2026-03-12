f8002a13bf7ca7ad9cb1a3567cf10bda81b72bcd
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-03-12 15:40:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg');
 
 // [1] КОНФИГУРАЦИЯ
 const BOT_TOKEN = "8745333905:AAGTuUyJmU2oHp5FXH98ky6IhP3jmAOttjw";
-const DOMAIN = "np.bothost.ru";
+const DOMAIN = "neural-pulse.bothost.ru";
 const PORT = process.env.PORT || 3000;
 const PG_URI = "postgresql://bothost_db_4405eff8747f:xqUdDdjCZViF1FqeU9jiWMqyd69boOTjHtHvjlcDmeM@node1.pghost.ru:32820/bothost_db_4405eff8747f";
 
