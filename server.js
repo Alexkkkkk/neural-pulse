12a566140047dce8a828ad05b10ae4df70a2ad7c
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-03-14 21:05:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { Pool } = require('pg');
 const cors = require('cors');
 
-const VERSION = "1.6.0";
+const VERSION = "1.6.1";
 const BOT_TOKEN = "8745333905:AAGTuUyJmU2oHp5FXH98ky6IhP3jmAOttjw";
 const PG_URI = "postgresql://bothost_db_4405eff8747f:xqUdDdjCZViF1FqeU9jiWMqyd69boOTjHtHvjlcDmeM@node1.pghost.ru:32820/bothost_db_4405eff8747f";
 
@@ -71,6 +71,6 @@ bot.start(async (ctx) => {
 });
 
 app.listen(3000, () => {
-    console.log(`[ v${VERSION} ] SERVER ACTIVE. PUBLIC ONLY.`);
+    console.log(`[ v${VERSION} ] SERVER STARTED. ALL SYSTEMS NOMINAL.`);
     bot.launch();
 });
